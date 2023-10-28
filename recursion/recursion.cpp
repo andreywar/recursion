2ddcbdade8d936ba0a92ceab1fca87da87fd7761
andreywar <andryusha6@mail.ru>
2023-10-29 01:13:27 +0300

    create repos

---
+++ recursion/recursion.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <time.h>
+using namespace std;
+int search(int* array, int max_number, int current_index, int size)
+{
+	int temp = 0;
+	if (array[current_index] > max_number) { temp = current_index; max_number = array[current_index]; }
+	if (current_index == size - 1) return temp;
+	int temp2 = search(array, max_number, current_index + 1, size);
+	if (temp < temp2) return temp2; else return temp;
+}
+
+int main()
+{
+	cout << "Enter size of array: ";
+	int N;
+	cin >> N;
+	int* a = new int[N];
+	srand(time(NULL));
+	for (int i = 0; i < N; i++)
+	{
+		a[i] = rand() % 100;
+		cout << a[i] << " ";
+	}
+	int max_index = search(a, a[0], 1, N);
+	cout << "\nMaximum element a[" << max_index << "]=" << a[max_index] << endl;
+	system("pause");
+	delete[] a;
+	return 0;
+
+
+}
