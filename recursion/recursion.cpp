f6c6a68d3a338b32eb8969f58fd01822e937578c
andreywar <andryusha6@mail.ru>
2023-10-29 02:08:14 +0300

    not working but create recursion for horse steps

--- recursion/recursion.cpp
+++ recursion/recursion.cpp
@@ -1,6 +1,50 @@
 ﻿#include <iostream>
 #include <time.h>
 using namespace std;
+
+
+
+const int n = 5;
+int a[8] = { 2, 1, -1, -2, -2, -1, 1, 2 };
+int b[8] = { 1, 2, 2, 1, -1, -2, -2, -1 };
+int h[n][n];
+int try0(int, int, int);
+
+void horse()
+{
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			h[i][j] = 0;
+	h[2][2] = 1;
+	if (try0(2, 2, 2))
+	{
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < n; j++) cout << " " << h[i][j];
+	}
+	else cout << "there not solution for that step" << endl;
+}
+
+
+
+int try0(int i, int x, int y)
+{
+	int u, v, k = 0, q1 = 0;
+	while (q1 == 0 && k < n)
+	{
+		u = x + a[k];
+		v = y + b[k];
+		k++;
+		if (u >= 0 && u < n && v >= 0 && v < n)
+			if (h[u][v] == 0)
+			{
+				h[u][v] = i;
+				if (i < n * n && try0(i + 1, u, v) == 0) h[u][v] = 0;
+				else q1 = 1;
+			}
+	}
+	return q1;
+}
+
 int search(int* array, int max_number, int current_index, int size)
 {
 	int temp = 0;
@@ -27,6 +71,8 @@ int main()
 	system("pause");
 	delete[] a;
 	return 0;
-
-
+	horse();
 }
+
+
+
